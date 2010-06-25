7a7ff315ca4ea7a1cadd4d89b0c8c24bb65ba80b
jankeesvanandel <jankeesvanandel@73f01b7c-da5f-99e1-4a80-a261836a9de2>
2010-06-25 20:29:45 +0000

    refactored video page javascript and added video controls functionality

--- web-html5/src/main/webapp/resources/parleys/video.js
+++ web-html5/src/main/webapp/resources/parleys/video.js
@@ -1,9 +1,3 @@
-//var videoPlayer;
-//var presentationSlides;
-//var videoNavigationBar;
-//var videoDuration;
-//var videoNavigationBarWidth;
-//var videoNavigationBarCursor;
 var isCursorDragging = false;
 
 var com;
@@ -19,6 +13,8 @@ com.parleys.html5client.MainVideoLoop = function() {
     this._observers = [];
     this._observerTypes = [];
     this._currentTime = 0;
+    this._currentChapter = 0;
+    this._chapters = [];
 
     this.notifyAllObservers = function(originType) {
         var timeChangedEvent = {
@@ -47,7 +43,21 @@ com.parleys.html5client.MainVideoLoop.prototype = {
             throw new Error("originType is not specified");
         }
         this._currentTime = time;
+
+        for (var i = 0; i < this._chapters.length; i++) {
+            var chapter = this._chapters[i];
+            if (chapter.startTime <= time
+             && chapter.endTime > time) {
+                this._currentChapter = i;
+                break;
+            }
+        }
+
         this.notifyAllObservers(originType);
+    },
+
+    setChapters: function(chapters) {
+        this._chapters = chapters;
     }
 };
 
@@ -62,24 +72,61 @@ $(document).ready(function() {
 
 window.onload = function() {
     initializeVideoEventSources();
-
     setInitialPosition();
+    disableTextSelection();
+    initializeControlsEventHandlers();
 
     $("#videoPlayer")[0].play();
 };
 
-function setInitialPosition() {
-    var time = 0;
+function initializeControlsEventHandlers() {
+    $("#prevChapterButton").click(function() {
+        var previousChapter = mainVideoLoop._currentChapter - 1;
+        var time = getStartTimeForChapter(previousChapter);
+        mainVideoLoop.timeChanged(time, OBSERVER_TYPE_PROGRESSBAR);
+        return false;
+    });
 
+    $("#nextChapterButton").click(function() {
+        var nextChapter = mainVideoLoop._currentChapter + 1;
+        var time = getStartTimeForChapter(nextChapter);
+        mainVideoLoop.timeChanged(time, OBSERVER_TYPE_PROGRESSBAR);
+        return false;
+    });
+
+    $("#playButton").click(function() {
+        $("#playButton").hide();
+        $("#pauzeButton").show();
+        $("#videoPlayer")[0].play();
+        return false;
+    });
+
+    $("#pauzeButton").click(function() {
+        $("#playButton").show();
+        $("#pauzeButton").hide();
+        $("#videoPlayer")[0].pause();
+        return false;
+    });
+}
+
+function disableTextSelection() {
+    document.onselectstart = function() {
+        return false; // ie
+    };
+    document.onmousedown = function() {
+        return false; // mozilla
+    };
+}
+
+function setInitialPosition() {
     var href = document.location.href;
     var indexOfAnchor = href.indexOf("#");
-    if (indexOfAnchor != -1) {
+    if (indexOfAnchor != -1 && indexOfAnchor < href.length - 1) {
         var slideId = href.substr(indexOfAnchor + 1);
         var slide = findSlideById(slideId);
-        time = parseFloat(slide.attr("startTime"));
+        var time = parseFloat(slide.attr("startTime"));
+        $("#videoPlayer")[0].currentTime = time;
     }
-
-    $("#videoPlayer")[0].currentTime = time;
 }
 
 function initializeProgressBarEventSource() {
@@ -87,6 +134,7 @@ function initializeProgressBarEventSource() {
         var href = $(this).attr("href");
         var id = href.substr(1);
         var slide = findSlideById(id);
+
         mainVideoLoop.timeChanged(slide.attr("startTime"), OBSERVER_TYPE_PROGRESSBAR);
     });
 }
@@ -100,7 +148,7 @@ function initializeProgressBarCursor() {
             isCursorDragging = true;
         },
         stop: function() {
-            var position = $("#videoNavigationBarCursor").position().left + 7;
+            var position = $("#videoNavigationBarCursor").position().left - (150 - 7);
             var items = $("#videoNavigationBar li");
             var lastItem = items.last();
             var totalWidth = parseFloat(lastItem.position().left + lastItem.width());
@@ -113,6 +161,28 @@ function initializeProgressBarCursor() {
         }
     });
 }
+
+function getStartTimeForChapter(chapterIndex) {
+    var presentationSlides = $("#slidesContainer img");
+
+    chapterIndex = max(0, chapterIndex);
+    chapterIndex = min(presentationSlides, chapterIndex);
+
+    var slide = $(presentationSlides[chapterIndex]);
+    return parseFloat(slide.attr("startTime"));
+}
+
+function getTimeFromCursorPosition() {
+    var position = $("#videoNavigationBarCursor").position().left - (150 - 7);
+    var items = $("#videoNavigationBar li");
+    var lastItem = items.last();
+    var totalWidth = parseFloat(lastItem.position().left + lastItem.width());
+    var totalDuration = parseFloat($("#slidesContainer img").last().attr("endTime"));
+    var positionInPercentage = position / totalWidth;
+    var time = positionInPercentage * totalDuration;
+    return time;
+}
+
 function updateVideo(timeChangedEvent) {
     $("#videoPlayer")[0].currentTime = timeChangedEvent.currentTime;
 }
@@ -122,6 +192,7 @@ function initializeVideoEventSource() {
         mainVideoLoop.timeChanged($("#videoPlayer")[0].currentTime, OBSERVER_TYPE_VIDEO);
     };
 }
+
 function initializeVideoEventSources() {
     initializeVideoEventSource();
     initializeProgressBarEventSource();
@@ -129,15 +200,10 @@ function initializeVideoEventSources() {
 }
 
 function updateSlide(timeChangedEvent) {
-    var now = timeChangedEvent.currentTime;
-    var searchingForSlide = true;
     var presentationSlides = $("#slidesContainer img");
     for (var i = 0; i < presentationSlides.length; i++) {
         var slide = $(presentationSlides[i]);
-        if (searchingForSlide
-         && now >= parseFloat(slide.attr("startTime"))
-         && now < parseFloat(slide.attr("endTime"))) {
-            searchingForSlide = false;
+        if (mainVideoLoop._currentChapter == i) {
             slide.css("display", "block");
         } else {
             slide.css("display", "none");
@@ -154,6 +220,8 @@ function updateProgressBar(timeChangedEvent) {
         var position = (now / videoDuration) * videoNavigationBarWidth;
         position -= 7; // Half the cursor width
         $("#videoNavigationBarCursor").css("left", position + "px");
+
+        updateVideoPositionIndicator(now);
     }
 }
 
@@ -164,13 +232,16 @@ function findSlideById(id) {
 function initializeVideoNavigationBar() {
     $("#videoNavigationBar").css("visibility", "hidden");
 
+    var chapters = [];
     var videoNavigationBar = $("#videoNavigationBar");
-    var videoNavigationBarWidth = videoNavigationBar.width();
+    var videoNavigationBarWidth = videoNavigationBar.width() - 150; // subtract the videoTimeAndPosition width
     var items = $("#videoNavigationBar li");
     var presentationSlides = $("#slidesContainer img");
     var noOfSlides = presentationSlides.length;
     var lastSlide = presentationSlides.last();
     var videoDuration = parseFloat(lastSlide.attr("endTime"));
+
+    updateVideoDurationIndicator(videoDuration);
 
     var lastEndTime = 0;
     for (var i = 0; i < noOfSlides; i++) {
@@ -185,9 +256,36 @@ function initializeVideoNavigationBar() {
         var width = durationPercentage * (videoNavigationBarWidth - totalBorderWidthsInPixels);
 
         $(items[i]).width(width);
+        chapters.push({
+            startTime: startTime,
+            endTime: endTime
+        });
     }
 
+    mainVideoLoop.setChapters(chapters);
+
     $("#videoNavigationBar").css("visibility", "visible");
+}
+
+function updateVideoDurationIndicator(videoDuration) {
+    var string = secondsToString(videoDuration);
+    $("#videoTimeAndPosition span.duration").text(string);
+}
+
+function updateVideoPositionIndicator(position) {
+    var string = secondsToString(position);
+    $("#videoTimeAndPosition span.position").text(string + " / ");
+}
+
+function secondsToString(videoDuration) {
+    var hours = parseInt((videoDuration / 3600) % 24);
+    if (hours < 10) hours = "0" + hours;
+    var minutes = parseInt((videoDuration / 60) % 60);
+    if (minutes < 10) minutes = "0" + minutes;
+    var seconds = parseInt(videoDuration % 60);
+    if (seconds < 10) seconds = "0" + seconds;
+
+    return hours + ":" + minutes + ":" + seconds;
 }
 
 function min(i1, i2) {
@@ -195,155 +293,12 @@ function min(i1, i2) {
     else return i2;
 }
 
+function max(i1, i2) {
+    if (i1 > i2) return i1;
+    else return i2;
+}
+
 function ontimeupdateHandler() {
     var now = $("#videoPlayer")[0].currentTime;
     mainVideoLoop.timeChanged(now, OBSERVER_TYPE_VIDEO);
 }
-
-
-//$(items[i]).find("a").click(function() {
-//    var slideId = $(this).attr("href").substr(1);
-//    jumpToSlideById(slideId);
-//    addAnchorToUrl(slideId);
-//    return false;
-//});
-
-
-//var anchorIndex = document.location.href.indexOf("#");
-//if (anchorIndex != -1) {
-//    var slideIdFromUrl = document.location.href.substring(anchorIndex+1);
-//    jumpToSlideById(slideIdFromUrl);
-//}
-
-
-
-//window.onload = function() {
-//    videoPlayer = $("#videoPlayer")[0];
-//    presentationSlides = $("img");
-//    videoNavigationBar = $("#videoNavigationBar");
-//    videoNavigationBarCursor = $("#videoNavigationBarCursor");
-//    initializeVideoNavigationBar();
-//
-//    // disable selection
-//    document.onselectstart = function() { return false; }; // IE
-//    document.onmousedown = function() { return false; }; // Mozilla
-//
-//    videoPlayer.play();
-//};
-//
-//function addAnchorToUrl(anchor) {
-//    var anchorIndex = document.location.href.indexOf("#");
-//    if (anchorIndex != -1) {
-//        document.location = document.location.href.substring(0, anchorIndex) + "#" + anchor;
-//    } else {
-//        document.location = document.location + "#" + anchor;
-//    }
-//}
-//function initializeVideoNavigationBar() {
-//    videoNavigationBar = $("#videoNavigationBar");
-//    videoNavigationBarWidth = videoNavigationBar.width();
-//    var items = $("#videoNavigationBar li");
-//    var noOfSlides = presentationSlides.length;
-//    var lastSlide = presentationSlides.last();
-//    videoDuration = parseFloat(lastSlide.attr("endTime"));
-//
-//    var lastEndTime = 0;
-//    for (var i = 0; i < noOfSlides; i++) {
-//        var slide = $(presentationSlides[i]);
-//        var startTime = parseFloat(slide.attr("startTime"));
-//        lastEndTime = startTime;
-//        var endTime = min(parseFloat(slide.attr("endTime")), videoDuration);
-//
-//        var chapterDuration = endTime - startTime;
-//        var durationPercentage = chapterDuration / videoDuration;
-//        var totalBorderWidthsInPixels = (noOfSlides * 2);
-//        var width = durationPercentage * (videoNavigationBarWidth - totalBorderWidthsInPixels);
-//
-//        $(items[i]).width(width);
-//
-//        $(items[i]).find("a").click(function() {
-//            var slideId = $(this).attr("href").substr(1);
-//            jumpToSlideById(slideId);
-//            addAnchorToUrl(slideId);
-//            return false;
-//        });
-//    }
-//
-//    videoNavigationBar.css("visibility", "visible");
-//
-//    videoNavigationBarCursor.draggable({
-//        axis: 'x',
-//        containment: 'parent',
-//        cursor: 'pointer',
-//        start: function(event, ui) {
-//            isDragging = true;
-//        },
-//        stop: function(event, ui) {
-//            jumpToSlideByPosition();
-//            isDragging = false;
-//        }
-//    });
-//
-//    var anchorIndex = document.location.href.indexOf("#");
-//    if (anchorIndex != -1) {
-//        var slideIdFromUrl = document.location.href.substring(anchorIndex+1);
-//        jumpToSlideById(slideIdFromUrl);
-//    }
-//}
-//
-//function jumpToSlideByPosition() {
-//    var position = videoNavigationBarCursor.position().left + 7;
-//    var items = $("#videoNavigationBar li");
-//    var lastItem = items.last();
-//    var totalWidth = parseFloat(lastItem.position().left + lastItem.width());
-//    var totalDuration = parseFloat(presentationSlides.last().attr("endTime"));
-//    var positionInPercentage = position / totalWidth;
-//    var position = positionInPercentage * totalDuration;
-//
-//    videoPlayer.currentTime = position;
-//    updateActiveSlide(false);
-//}
-//
-//function updateActiveSlide(shouldMoveCursor) {
-//    if (shouldMoveCursor) {
-//        moveCursor();
-//    }
-//
-//    var now = videoPlayer.currentTime;
-//    var searchingForSlide = true;
-//    for (var i = 0; i < presentationSlides.length; i++) {
-//        var slide = $(presentationSlides[i]);
-//        if (searchingForSlide
-//                && now >= slide.attr("startTime")
-//                && now < slide.attr("endTime")) {
-//            searchingForSlide = false;
-//            slide.css("display", "block");
-//        } else {
-//            slide.css("display", "none");
-//        }
-//    }
-//}
-//
-//function moveCursor() {
-//    if (!isDragging) {
-//        var now = videoPlayer.currentTime;
-//        var position = (videoPlayer.currentTime / videoDuration) * videoNavigationBarWidth;
-//        position -= 7; // Half the cursor width
-//        videoNavigationBarCursor.css("left", position + "px");
-//    }
-//}
-//
-//function jumpToSlideById(slideId) {
-//    var slide;
-//    for (var i = 0; i < presentationSlides.length; i++) {
-//        if (presentationSlides[i].id == "slide_" + slideId) {
-//            slide = presentationSlides[i];
-//            break;
-//        }
-//    }
-//
-//    if (slide) {
-//        updateActiveSlide(true);
-//        videoPlayer.currentTime = parseFloat($(slide).attr("startTime"));
-//    }
-//}
