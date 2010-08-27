26e8de0606da0c190acaecb7e3330938eb5087b9
sja@parleys.com <sja@parleys.com@73f01b7c-da5f-99e1-4a80-a261836a9de2>
2010-08-27 09:03:51 +0000

    - Introduced new iPad L&F in overview pages (still to do is the navigation bar)

--- trunk/web-html5/src/main/webapp/resources/parleys/playerAnimation.js
+++ trunk/web-html5/src/main/webapp/resources/parleys/playerAnimation.js
@@ -14,8 +14,6 @@
  * limitations under the License.
  */
 
-
-
 var currentState = "EQUAL";
 
 var STATE_EQUAL = "EQUAL";
@@ -44,273 +42,236 @@ var inner;
 $(window).bind('resize', onResize);
 
 $(document).bind('ready', function() {
-  $("#videoContainer").single_double_click(onVideoClick,onVideoDoubleClick);
-  $("#slidesContainer").single_double_click(onSlideClick,onSlideDoubleClick);
+    $("#videoContainer").single_double_click(onVideoClick, onVideoDoubleClick);
+    $("#slidesContainer").single_double_click(onSlideClick, onSlideDoubleClick);
 
-    
     //$("#videoContainer").bind('gesturestart',onVideoDoubleClick);
 
+    slideOriginalRect.x = 0;
+    slideOriginalRect.y = 0;
+    slideOriginalRect.width = 400;
+    slideOriginalRect.height = 300;
 
+    videoOriginalRect.x = 0;
+    videoOriginalRect.y = 0;
+    videoOriginalRect.width = 400;
+    videoOriginalRect.height = 300;
 
-  slideOriginalRect.x = 0;
-  slideOriginalRect.y = 0;
-  slideOriginalRect.width = 400;
-  slideOriginalRect.height = 300;
-    
+    v = $("#videoContainer");
+    s = $("#slidesContainer");
+    inner = $("#inner");
+    agenda = $("#agenda");
 
-  videoOriginalRect.x = 0;
-  videoOriginalRect.y = 0;
-  videoOriginalRect.width = 400;
-  videoOriginalRect.height = 300;
+    oldWidth = $("#mainContent").width();
 
-  v = $("#videoContainer");
-  s = $("#slidesContainer");
-  inner = $("#inner");
-  agenda = $("#agenda");
-
-
-  oldWidth = $("#mainContent").width();
-
-  setInterval(checkSize,2000);
-  actuallyResize();
+    setInterval(checkSize, 2000);
+    actuallyResize();
     allowZoom(false);
-
 });
 
-
-
 function allowZoom(flag) {
-  if (flag == true) {
-    $('head meta[name=viewport]').remove();
-    $('head').prepend('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=10.0, minimum-scale=1, user-scalable=1" />');
-  } else {
-    $('head meta[name=viewport]').remove();
-    $('head').prepend('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=0" />');
-  }
+    if (flag) {
+        $('head meta[name=viewport]').remove();
+        $('head').prepend('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=10.0, minimum-scale=1, user-scalable=1" />');
+    } else {
+        $('head meta[name=viewport]').remove();
+        $('head').prepend('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=0" />');
+    }
 }
 
-
-
 var oldWidth = 0;
-function checkSize(){
-    if($("#mainContent").width()!=oldWidth){
-             actuallyResize();
+function checkSize() {
+    if ($("#mainContent").width() != oldWidth) {
+        actuallyResize();
     }
 
     oldWidth = $("#mainContent").width();
 }
 
-
-function onVideoDoubleClick(){
-	if(currentState == STATE_VIDEO_FULL){
-		currentState = STATE_EQUAL;
-	}else{
-		currentState = STATE_VIDEO_FULL;
-	}
-	changeState();
+function onVideoDoubleClick() {
+    if (currentState == STATE_VIDEO_FULL) {
+        currentState = STATE_EQUAL;
+    } else {
+        currentState = STATE_VIDEO_FULL;
+    }
+    changeState();
 }
 
-function onSlideDoubleClick(){
-	if(currentState == STATE_SLIDE_FULL){
-		currentState = STATE_EQUAL;
-	}else{
-		currentState = STATE_SLIDE_FULL;
-	}
-	changeState();
+function onSlideDoubleClick() {
+    if (currentState == STATE_SLIDE_FULL) {
+        currentState = STATE_EQUAL;
+    } else {
+        currentState = STATE_SLIDE_FULL;
+    }
+    changeState();
 }
 
-function onVideoClick(){
-	//alert("video click");
-	if (currentState != STATE_VIDEO_BIG) {
-		currentState = STATE_VIDEO_BIG;
-	}else{
-		currentState = STATE_EQUAL;
-	}
-
-	changeState();
+function onVideoClick() {
+    //alert("video click");
+    if (currentState != STATE_VIDEO_BIG) {
+        currentState = STATE_VIDEO_BIG;
+    } else {
+        currentState = STATE_EQUAL;
+    }
+    changeState();
 }
 
-function onSlideClick(){
-	//alert("slide click");
-	if (currentState != STATE_SLIDE_BIG) {
-		currentState = STATE_SLIDE_BIG;
-	}else{
-		currentState = STATE_EQUAL;
-	}
-
-	changeState();
+function onSlideClick() {
+    //alert("slide click");
+    if (currentState != STATE_SLIDE_BIG) {
+        currentState = STATE_SLIDE_BIG;
+    } else {
+        currentState = STATE_EQUAL;
+    }
+    changeState();
 }
 
-
-
-function changeState(){
-	if (currentState == STATE_EQUAL) {
-	s.css("opacity","100");
-	v.css("opacity","100");
-  	applyRect(videoEqualRect,v);
-	applyRect(slideEqualRect,s);
-  }else if(currentState == STATE_VIDEO_BIG){
-  	s.css("opacity","100");
-	v.css("opacity","100");
-  	applyRect(videoBigRect,v);
-	applyRect(slideSmallRect,s);
-  }else if(currentState == STATE_SLIDE_BIG){
-  	s.css("opacity","100");
-	v.css("opacity","100");
-  	applyRect(videoSmallRect,v);
-	applyRect(slideBigRect,s);
-  }else if(currentState == STATE_SLIDE_FULL){
-  	s.css("opacity","100");
-	v.css("opacity","0");
-	applyRect(slideFullRect,s);
-  }else if(currentState == STATE_VIDEO_FULL){
-  	s.css("opacity","0");
-	v.css("opacity","100");
-  	applyRect(videoFullRect,v);
-  }
+function changeState() {
+    if (currentState == STATE_EQUAL) {
+        s.css("opacity", "100");
+        v.css("opacity", "100");
+        applyRect(videoEqualRect, v);
+        applyRect(slideEqualRect, s);
+    } else if (currentState == STATE_VIDEO_BIG) {
+        s.css("opacity", "100");
+        v.css("opacity", "100");
+        applyRect(videoBigRect, v);
+        applyRect(slideSmallRect, s);
+    } else if (currentState == STATE_SLIDE_BIG) {
+        s.css("opacity", "100");
+        v.css("opacity", "100");
+        applyRect(videoSmallRect, v);
+        applyRect(slideBigRect, s);
+    } else if (currentState == STATE_SLIDE_FULL) {
+        s.css("opacity", "100");
+        v.css("opacity", "0");
+        applyRect(slideFullRect, s);
+    } else if (currentState == STATE_VIDEO_FULL) {
+        s.css("opacity", "0");
+        v.css("opacity", "100");
+        applyRect(videoFullRect, v);
+    }
 }
-
-
 
 var resizeTimer;
 
-function onResize(){
+function onResize() {
     var contentWidth = $("#mainContent").width();
-	var contentHeight = $("#mainContent").height();
+    var contentHeight = $("#mainContent").height();
     console.log("onResize: " + contentWidth + "x" + contentHeight);
     clearTimeout(resizeTimer);
-     resizeTimer = setTimeout(actuallyResize,200);
-
-
+    resizeTimer = setTimeout(actuallyResize, 200);
 }
 
-
-function actuallyResize(){
+function actuallyResize() {
     var contentWidth = $("#mainContent").width();
-	var contentHeight = $("#mainContent").height();
+    var contentHeight = $("#mainContent").height();
 
-	var contentEqualRight = new Object();
+    var contentEqualRight = new Object();
     var contentEqualLeft = new Object();
     var contentBig = new Object();
     var contentSmall = new Object();
     var contentFull = new Object();
 
-    if(contentWidth>contentHeight){
+    if (contentWidth > contentHeight) {
         contentEqualRight.x = contentWidth / 2;
         contentEqualRight.y = 0;
         contentEqualRight.width = contentWidth / 2;
         contentEqualRight.height = contentHeight;
-
 
         contentEqualLeft.x = 0;
         contentEqualLeft.y = 0;
         contentEqualLeft.width = contentWidth / 2;
         contentEqualLeft.height = contentHeight;
 
-
         contentBig.x = contentWidth * 0.2;
         contentBig.y = 0;
         contentBig.width = contentWidth * 0.8;
         contentBig.height = contentHeight;
-
 
         contentSmall.x = 0;
         contentSmall.y = 0;
         contentSmall.width = contentWidth * 0.2;
         contentSmall.height = contentHeight;
 
-    }else{
+    } else {
         contentEqualRight.x = 0;
-	    contentEqualRight.y = 0;
-	    contentEqualRight.width = contentWidth;
-	    contentEqualRight.height = contentHeight/2;
+        contentEqualRight.y = 0;
+        contentEqualRight.width = contentWidth;
+        contentEqualRight.height = contentHeight / 2;
 
+        contentEqualLeft.x = 0;
+        contentEqualLeft.y = contentHeight / 2;
+        contentEqualLeft.width = contentWidth;
+        contentEqualLeft.height = contentHeight / 2;
 
-	    contentEqualLeft.x = 0;
-	    contentEqualLeft.y = contentHeight/2;
-	    contentEqualLeft.width = contentWidth;
-	    contentEqualLeft.height = contentHeight/2;
+        contentBig.x = contentWidth * 0.2;
+        contentBig.y = 0;
+        contentBig.width = contentWidth * 0.8;
+        contentBig.height = contentHeight;
 
-
-	    contentBig.x = contentWidth * 0.2;
-	    contentBig.y = 0;
-	    contentBig.width = contentWidth * 0.8;
-	    contentBig.height = contentHeight;
-
-
-	    contentSmall.x = 0;
-	    contentSmall.y = 0;
-	    contentSmall.width = contentWidth * 0.2;
-	    contentSmall.height = contentHeight;
-
+        contentSmall.x = 0;
+        contentSmall.y = 0;
+        contentSmall.width = contentWidth * 0.2;
+        contentSmall.height = contentHeight;
     }
 
-	contentFull.x = 0;
-	contentFull.y = 0;
-	contentFull.width = contentWidth;
-	contentFull.height = contentHeight;
+    contentFull.x = 0;
+    contentFull.y = 0;
+    contentFull.width = contentWidth;
+    contentFull.height = contentHeight;
 
+    slideEqualRect = aspectFit(contentEqualLeft, slideOriginalRect);
+    videoEqualRect = aspectFit(contentEqualRight, videoOriginalRect);
+    slideSmallRect = aspectFit(contentSmall, slideOriginalRect);
+    slideSmallRect.y = 0;
+    videoSmallRect = aspectFit(contentSmall, videoOriginalRect);
+    videoSmallRect.y = 0;
+    slideBigRect = aspectFit(contentBig, slideOriginalRect);
+    videoBigRect = aspectFit(contentBig, videoOriginalRect);
+    slideFullRect = aspectFit(contentFull, slideOriginalRect);
+    videoFullRect = aspectFit(contentFull, videoOriginalRect);
 
-	slideEqualRect = aspectFit(contentEqualLeft, slideOriginalRect);
-	videoEqualRect = aspectFit(contentEqualRight, videoOriginalRect);
-	slideSmallRect = aspectFit(contentSmall, slideOriginalRect);
-	slideSmallRect.y = 0;
-	videoSmallRect = aspectFit(contentSmall, videoOriginalRect);
-	videoSmallRect.y = 0;
-	slideBigRect = aspectFit(contentBig, slideOriginalRect);
-	videoBigRect = aspectFit(contentBig, videoOriginalRect);
-	slideFullRect = aspectFit(contentFull, slideOriginalRect);
-	videoFullRect = aspectFit(contentFull, videoOriginalRect);
+    var slideSmallUsedWidth = slideSmallRect.width + videoBigRect.width;
+    var slideSmallStartX = contentWidth / 2 - slideSmallUsedWidth / 2;
+    slideSmallRect.x = slideSmallStartX;
+    videoBigRect.x = slideSmallStartX + slideSmallRect.width;
 
-	var slideSmallUsedWidth = slideSmallRect.width + videoBigRect.width;
-	var slideSmallStartX = contentWidth/2 - slideSmallUsedWidth/2;
-	slideSmallRect.x = slideSmallStartX;
-	videoBigRect.x = slideSmallStartX+slideSmallRect.width;
+    var videoSmallUsedWidth = videoSmallRect.width + slideBigRect.width;
+    var videoSmallStartX = contentWidth / 2 - videoSmallUsedWidth / 2;
+    videoSmallRect.x = videoSmallStartX;
+    slideBigRect.x = videoSmallStartX + videoSmallRect.width;
 
+    if (currentState == STATE_EQUAL) {
+        applyRect(videoEqualRect, v);
+        applyRect(slideEqualRect, s);
+    }
+    else if (currentState == STATE_VIDEO_BIG) {
+        applyRect(videoBigRect, v);
+        applyRect(slideSmallRect, s);
+    }
+    else if (currentState == STATE_SLIDE_BIG) {
+        applyRect(videoSmallRect, v);
+        applyRect(slideBigRect, s);
+    }
+    else if (currentState == STATE_SLIDE_FULL) {
+        applyRect(slideFullRect, s);
+    }
+    else if (currentState == STATE_VIDEO_FULL) {
+        applyRect(videoFullRect, v);
+    }
 
-	var videoSmallUsedWidth = videoSmallRect.width + slideBigRect.width;
-	var videoSmallStartX = contentWidth/2 - videoSmallUsedWidth/2;
-	videoSmallRect.x = videoSmallStartX;
-	slideBigRect.x = videoSmallStartX+videoSmallRect.width;
-
-
-
-	if (currentState == STATE_EQUAL) {
-		applyRect(videoEqualRect, v);
-		applyRect(slideEqualRect, s);
-	}
-	else
-		if (currentState == STATE_VIDEO_BIG) {
-			applyRect(videoBigRect, v);
-			applyRect(slideSmallRect, s);
-		}
-		else
-			if (currentState == STATE_SLIDE_BIG) {
-				applyRect(videoSmallRect, v);
-				applyRect(slideBigRect, s);
-			}
-			else
-				if (currentState == STATE_SLIDE_FULL) {
-					applyRect(slideFullRect, s);
-				}
-				else
-					if (currentState == STATE_VIDEO_FULL) {
-						applyRect(videoFullRect, v);
-					}
-
-
-//	var agenda = document.getElementById('agenda');
-
-	if (currentState == STATE_EQUAL) {
-		agenda.css("left", window.innerWidth / 2 - 190 + "px");
-	}else{
-		agenda.css("left", window.innerWidth / 2 - 400 + "px");
-	}
+    //	var agenda = document.getElementById('agenda');
+    if (currentState == STATE_EQUAL) {
+        agenda.css("left", window.innerWidth / 2 - 190 + "px");
+    } else {
+        agenda.css("left", window.innerWidth / 2 - 400 + "px");
+    }
 
     resizeChapters();
 }
 
-function resizeChapters(){
+function resizeChapters() {
     var chapters = $("#chapters div").not("#videoNavigationBarCursor");
     var chapterContainerWidth = $("#chapters").width();
     var noOfChapters = chapters.length;
@@ -321,95 +282,90 @@ function resizeChapters(){
         var cuepoint = parseFloat(chapter.attr("cuepoint"));
         var duration = parseFloat(chapter.attr("duration"));
 
-        var percentage = duration/totalDuration;
-        var chapterWidth = chapterContainerWidth*percentage;
-        chapter.css("width",chapterWidth-1+"px");
-        chapter.css("left",startX+"px");
-        startX+=chapterWidth;
+        var percentage = duration / totalDuration;
+        var chapterWidth = chapterContainerWidth * percentage;
+        chapter.css("width", chapterWidth - 1 + "px");
+        chapter.css("left", startX + "px");
+        startX += chapterWidth;
     }
 }
 
-function applyRect(rect,item){
-	item.css("left", rect.x + "px")
-	item.css("top", rect.y + "px")
-  	item.css("width", rect.width + "px")
-  	item.css("height", rect.height + "px")
+function applyRect(rect, item) {
+    item.css("left", rect.x + "px");
+    item.css("top", rect.y + "px");
+    item.css("width", rect.width + "px");
+    item.css("height", rect.height + "px")
 }
 
-function aspectFit(outer,inner){
-		fixedRatio = outer.width/outer.height;
-		innerRatio = inner.width/inner.height;
-		newRect = new Object();
-		if(innerRatio >= fixedRatio){
-			newRect.width = outer.width;
-			newRect.height = inner.height/(inner.width/outer.width);
-		}else{
-			newRect.height = outer.height;
-			newRect.width = inner.width/(inner.height/outer.height);
-		}
-		newRect.x =outer.x + ( outer.width/2-newRect.width/2);
-		newRect.y = outer.y + ( outer.height/2-newRect.height/2);
-		return newRect;
+function aspectFit(outer, inner) {
+    var fixedRatio = outer.width / outer.height;
+    var innerRatio = inner.width / inner.height;
+    var newRect = new Object();
+    if (innerRatio >= fixedRatio) {
+        newRect.width = outer.width;
+        newRect.height = inner.height / (inner.width / outer.width);
+    } else {
+        newRect.height = outer.height;
+        newRect.width = inner.width / (inner.height / outer.height);
+    }
+    newRect.x = outer.x + ( outer.width / 2 - newRect.width / 2);
+    newRect.y = outer.y + ( outer.height / 2 - newRect.height / 2);
+    return newRect;
 }
 
-function toggleAgenda(){
+function toggleAgenda() {
 
-	 if (currentState == STATE_EQUAL) {
-		agenda.css("left",window.innerWidth / 2 - 190 + "px");
+    if (currentState == STATE_EQUAL) {
+        agenda.css("left", window.innerWidth / 2 - 190 + "px");
+    } else {
+        agenda.css("left", window.innerWidth / 2 - 400 + "px");
+    }
 
-	}else{
-		agenda.css("left",window.innerWidth / 2 - 400 + "px");
-	}
-
-	if (agenda_btn.value=="Show Agenda"){
-
-        agenda.css("display","block");
-		if (currentState == STATE_EQUAL) {
-			v.css("webkitTransform", "rotateY(-45deg) translateZ(-250px) translate(100px,0)");
-			s.css("webkitTransform", "rotateY(45deg) translateZ(-250px) translate(-100px,0)");
-		}else{
-			inner.css("webkitTransform", "rotateY(-45deg) translateZ(-500px) translate(100px,0)");
-		}
-		agenda.css("opacity","100");
-		 if (currentState == STATE_EQUAL) {
-		 	agenda.css("webkitTransform","rotateY(0deg) translateZ(0px)");
-		 }else{
-		 	agenda.css("webkitTransform","rotateY(20deg) translateZ(-200px)");
-		 }
-        agenda_btn.value="Hide Agenda";
-    }else{
-		setTimeout(hideAgenda,600);
-		v.css("webkitTransform","rotateY(0deg)");
-		s.css("webkitTransform","rotateY(0deg)");
-		inner.css("webkitTransform","rotateY(0deg) translateZ(0px)");
-		agenda_btn.value="Show Agenda";
-		agenda.css("opacity","0");
-		agenda.css("webkitTransform","rotateY(0deg) translateZ(500px)");
-	}
-
-
+    if (agenda_btn.value == "Show Agenda") {
+        agenda.css("display", "block");
+        if (currentState == STATE_EQUAL) {
+            v.css("webkitTransform", "rotateY(-45deg) translateZ(-250px) translate(100px,0)");
+            s.css("webkitTransform", "rotateY(45deg) translateZ(-250px) translate(-100px,0)");
+        } else {
+            inner.css("webkitTransform", "rotateY(-45deg) translateZ(-500px) translate(100px,0)");
+        }
+        agenda.css("opacity", "100");
+        if (currentState == STATE_EQUAL) {
+            agenda.css("webkitTransform", "rotateY(0deg) translateZ(0px)");
+        } else {
+            agenda.css("webkitTransform", "rotateY(20deg) translateZ(-200px)");
+        }
+        agenda_btn.value = "Hide Agenda";
+    } else {
+        setTimeout(hideAgenda, 600);
+        v.css("webkitTransform", "rotateY(0deg)");
+        s.css("webkitTransform", "rotateY(0deg)");
+        inner.css("webkitTransform", "rotateY(0deg) translateZ(0px)");
+        agenda_btn.value = "Show Agenda";
+        agenda.css("opacity", "0");
+        agenda.css("webkitTransform", "rotateY(0deg) translateZ(500px)");
+    }
 }
 
-function hideAgenda(){
-
-    agenda.css("display","none");
+function hideAgenda() {
+    agenda.css("display", "none");
 }
 
 jQuery.fn.single_double_click = function(single_click_callback, double_click_callback, timeout) {
-  return this.each(function(){
-    var clicks = 0, self = this;
-    jQuery(this).click(function(event){
-      clicks++;
-      if (clicks == 1) {
-        setTimeout(function(){
-          if(clicks == 1) {
-            single_click_callback.call(self, event);
-          } else {
-            double_click_callback.call(self, event);
-          }
-          clicks = 0;
-        }, timeout || 300);
-      }
+    return this.each(function() {
+        var clicks = 0, self = this;
+        jQuery(this).click(function(event) {
+            clicks++;
+            if (clicks == 1) {
+                setTimeout(function() {
+                    if (clicks == 1) {
+                        single_click_callback.call(self, event);
+                    } else {
+                        double_click_callback.call(self, event);
+                    }
+                    clicks = 0;
+                }, timeout || 300);
+            }
+        });
     });
-  });
-}
+};
