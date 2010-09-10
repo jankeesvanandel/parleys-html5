9adf9bb7f7511861477732f498a8680475af46b3
jankeesvanandel@gmail.com <jankeesvanandel@gmail.com@73f01b7c-da5f-99e1-4a80-a261836a9de2>
2010-09-10 19:34:34 +0000

    first big refactoring step: Split frontend into frontend-html5, frontend-ipad and frontend-shared

---
+++ web-shared/src/main/java/com/parleys/server/frontend/web/shared/util/ParleysExceptionHandlerFactory.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2010 Parleys.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.parleys.server.frontend.web.shared.util;
+
+import javax.faces.context.ExceptionHandler;
+import javax.faces.context.ExceptionHandlerFactory;
+
+/**
+ * Factory for our custom ParleysExceptionHandler implementation.
+ *
+ * @author Jan-Kees van Andel
+ */
+public class ParleysExceptionHandlerFactory extends ExceptionHandlerFactory {
+
+    private final ExceptionHandlerFactory wrapped;
+
+    /**
+     * Full constructor. Called by JSF.
+     *
+     * @param wrapped The wrapped ExceptionHandlerFactory implementation.
+     */
+    public ParleysExceptionHandlerFactory(ExceptionHandlerFactory wrapped) {
+        this.wrapped = wrapped;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public ExceptionHandler getExceptionHandler() {
+        ExceptionHandler wrappedExceptionHandler = wrapped.getExceptionHandler();
+        return new ParleysExceptionHandler(wrappedExceptionHandler);
+    }
+}
