038ebe31e7e4ef609bc2cdc067d56552d2353043
jankeesvanandel@gmail.com <jankeesvanandel@gmail.com@73f01b7c-da5f-99e1-4a80-a261836a9de2>
2010-09-13 09:58:57 +0000

    unescaped links in overview texts and disabled their link action, also made the entire rows clickable

--- trunk/web-html5/src/main/webapp/resources/parleys/paging.js
+++ trunk/web-html5/src/main/webapp/resources/parleys/paging.js
@@ -13,12 +13,39 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+$(document).ready(initializeClickableList);
+
+function initializeClickableList() {
+    $("div.overviewList li").each(function() {
+        // Disable links in the text
+        $(this).find(".overviewListText a").click(function() {
+            // Redirect the link to the same location as the rest of the row
+            var newLocation = $(this).closest("li").find(".overviewListThumbnail a").attr("href");
+            document.location = newLocation;
+
+            // Disable default link action
+            return false;
+        });
+
+        // Make each row behave like a link
+        $(this).click(function() {
+            var newLocation = $(this).find(".overviewListThumbnail a").attr("href");
+            document.location = newLocation;
+        });
+
+        // And make the rows look like links
+        $(this).css("cursor", "pointer");
+    });
+}
+
 function showInlineLoader() {
     $('img.inlineLoader').show();
 }
 
 function onPagingAjaxEvent(evt) {
-    if (evt.status == 'complete') {
+    if (evt.status == 'success') {
         $('img.inlineLoader').hide();
+        initializeClickableList();
     }
 }
