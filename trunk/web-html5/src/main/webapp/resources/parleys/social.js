2101d140b26876c1b1359ee8425fd3b06ab3c640
benjamin.dobler@googlemail.com <benjamin.dobler@googlemail.com@73f01b7c-da5f-99e1-4a80-a261836a9de2>
2010-09-14 16:03:54 +0000

    - added tabbar (jquery ui) for related/info/share functionality

---
+++ trunk/web-html5/src/main/webapp/resources/parleys/social.js
@@ -0,0 +1,116 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: dobler
+ * Date: 14.09.2010
+ * Time: 13:00:14
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+
+
+        var DIGG = "digg";
+		var FACEBOOK = "faceBook";
+		var GOOGLE_BOOKMARK = "googleBookmark";
+		var MYSPACE = "myspace";
+		var STUMBLE_UPON = "stumbleUpon";
+		var TECHNORATI = "technorati";
+		var TWITTER = "twitter";
+		var YAHOO_BOOKMARK = "yahooBookmark";
+
+
+        function post(to, link, title) {
+            var targetURL = "";
+			switch (to) {
+				case DIGG :
+					targetURL = "http://digg.com/submit?phase=2&url=" + link + "&title=" + title;
+					break;
+				case FACEBOOK :
+					targetURL = "http://www.facebook.com/sharer.php?u=" + link+"&t="+title;
+					break;
+				case GOOGLE_BOOKMARK :
+					targetURL = "http://www.google.com/bookmarks/mark?op=add&bkmk=" + link + "&title=" + title;
+					break;
+				case MYSPACE :
+					targetURL = "http://www.myspace.com/Modules/PostTo/Pages/?u=" + link + "&t=" + title;
+					break;
+				case STUMBLE_UPON :
+					targetURL = "http://www.stumbleupon.com/submit?url=" + link;
+					break;
+				case TECHNORATI :
+					targetURL = "http://technorati.com/faves/?add=" + link;
+					break;
+				case TWITTER :
+					targetURL = "http://twitter.com/home?status=" + link +" "+title;
+					break;
+				case YAHOO_BOOKMARK :
+					targetURL = "http://myweb2.search.yahoo.com/myresults/bookmarklet?u=" + link + "&t=" + title;
+					break;
+				default :
+					break;
+			}
+
+           // alert("Target: "+targetURL+"    "+to+"    "+link);
+            var load = window.open(targetURL,'','scrollbars=no,menubar=no,height=600,width=800,resizable=yes,toolbar=no,location=no,status=no')
+            //location.href = targetURL;
+		}
+
+
+var presentationId;
+var presentationTitle;
+
+
+
+
+$(document).bind('ready', function() {
+
+
+    $("#tabs").tabs();
+
+
+    $('#tabs').bind('tabsselect', function(event, ui) {
+
+        if(ui.index == 2){
+          $("#tabs").css("left", "0px");
+          $("#tabs").css("width", "700px");
+        } else{
+            $("#tabs").css("left", "0px");
+            $("#tabs").css("width", "550px");
+        }
+
+    });
+
+
+
+    presentationId =   $("#mainContent").attr("presentationId");
+    presentationTitle =  $("#mainContent").attr("presentationTitle");
+
+    $("#shareButton").click(function() {
+        toggleShare();
+    });
+
+
+     $("#share a").bind("click", function() {
+        var type = $(this).attr("type");
+        var link = "http://www.parleys.com/d/"+presentationId;
+
+        post(type,link,presentationTitle);
+        return false;
+    });
+
+
+} );
+
+
+var isShareOpen = false;
+function toggleShare(){
+  if(isShareOpen){
+      $("#tabs").css("left", "-710px");
+      isShareOpen = false;
+  }else{
+      $("#tabs").css("left", "0px");
+      $("#tabs").css("width", "550px"); 
+      isShareOpen = true;
+  }
+}
+
